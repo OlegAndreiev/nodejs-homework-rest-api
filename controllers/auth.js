47ef70723b632619cbedbba469aee6f88eadebf9
Oleg Andreiev <mr.olegandreev@gmail.com>
2023-03-19 00:14:22 +0200

    For mentor`s check

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,7 +50,7 @@ const verifyEmail = async (req, res) => {
   const { verificationCode } = req.params;
   const user = await User.findOne({ verificationCode });
   if (!user) {
-    throw HttpError(401, "Email not found");
+    throw HttpError(404, "User not found");
   }
   await User.findByIdAndUpdate(user._id, {
     verify: true,
@@ -58,7 +58,7 @@ const verifyEmail = async (req, res) => {
   });
 
   res.json({
-    message: "Email verify success",
+    message: "Verification successful",
   });
 };
 
@@ -66,10 +66,10 @@ const resendVerifyEmail = async (req, res) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw HttpError(401, "Email not found");
+    throw HttpError(404, "Email not found");
   }
   if (user.verify) {
-    throw HttpError(401, "Email already verify");
+    throw HttpError(400, "Verification has already been passed");
   }
 
   const verifyEmail = {
@@ -81,7 +81,7 @@ const resendVerifyEmail = async (req, res) => {
   await sendEmail(verifyEmail);
 
   res.json({
-    message: "Verify email send success",
+    message: "Verification email sent",
   });
 };
 
